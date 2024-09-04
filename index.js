fca4a2ef3857705e5849402d4fafd46f320611e2
DachSec <vladdad4298@gmail.com>
2024-09-04 23:11:27 +0200

    added index.css, 70-80% done index.html, and some js

---
+++ index.js
@@ -0,0 +1,7 @@
+<script>
+  function LanguageChange(1) {
+    const selectElement = document.getElementById('language');
+    const selectlanguage = selectElement.value;
+    alert('Language set: ' + selectedlanguage);
+  }
+</script>■
